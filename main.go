c5237cff7e2444c96d177a6440de51a3ed3250f0
Isaac Mackle <isaac.mackle@gmail.com>
2023-09-25 11:56:34 +1300

    ✨Introduce task queue to batch github action rebuilds

--- main.go
+++ main.go
@@ -4,16 +4,28 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
 )
 
+type Task struct {
+	ID     int
+	Action func()
+}
+
 const (
 	links_collection = "links"
+	links_task_id    = 1
 )
+
+var mutex sync.Mutex
+var taskQueue = make(map[int]*Task)
 
 func main() {
 	docsRepoGHWebhookToken := os.Getenv("DOCS_REPO_GH_WEBHOOK_TOKEN")
@@ -21,17 +33,20 @@ func main() {
 	app := pocketbase.New()
 
 	app.OnRecordAfterCreateRequest(links_collection).Add(func(e *core.RecordCreateEvent) error {
-		triggerGitHubWorkflow(docsRepoGHWebhookUrl, docsRepoGHWebhookToken)
+		scheduleTask(links_task_id, func() { triggerGitHubWorkflow(docsRepoGHWebhookUrl, docsRepoGHWebhookToken) })
+
 		return nil
 	})
 
 	app.OnRecordAfterUpdateRequest(links_collection).Add(func(e *core.RecordUpdateEvent) error {
-		triggerGitHubWorkflow(docsRepoGHWebhookUrl, docsRepoGHWebhookToken)
+		scheduleTask(links_task_id, func() { triggerGitHubWorkflow(docsRepoGHWebhookUrl, docsRepoGHWebhookToken) })
+
 		return nil
 	})
 
 	app.OnRecordAfterDeleteRequest(links_collection).Add(func(e *core.RecordDeleteEvent) error {
-		triggerGitHubWorkflow(docsRepoGHWebhookUrl, docsRepoGHWebhookToken)
+		scheduleTask(links_task_id, func() { triggerGitHubWorkflow(docsRepoGHWebhookUrl, docsRepoGHWebhookToken) })
+
 		return nil
 	})
 
@@ -49,7 +64,7 @@ func triggerGitHubWorkflow(url, token string) {
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
 	if err != nil {
-		fmt.Println("Error creating request:", err)
+		slog.Error("Error creating request:", "error", err)
 		return
 	}
 
@@ -60,8 +75,32 @@ func triggerGitHubWorkflow(url, token string) {
 
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error sending request:", err)
+		slog.Error("Error sending request:", "error", err)
 		return
 	}
 	defer resp.Body.Close()
+}
+
+func scheduleTask(taskID int, taskFunc func()) {
+	duration := 5 * time.Minute
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if existingTask, exists := taskQueue[taskID]; exists {
+		slog.Info("Task already exists, resetting the timer:", "taskID", taskID)
+		existingTask.Action = taskFunc
+	} else {
+		slog.Info("Creating new task:", "taskID", taskID)
+		task := &Task{ID: taskID, Action: taskFunc}
+		taskQueue[taskID] = task
+
+		go func() {
+			<-time.After(duration)
+			slog.Info("Running task:", "taskID", taskID)
+			task.Action()
+			mutex.Lock()
+			defer mutex.Unlock()
+			delete(taskQueue, taskID)
+		}()
+	}
 }
